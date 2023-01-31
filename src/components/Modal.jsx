4ff2ce2cfd531c8bc338f0c0f525fddc8a244f09
storm2107 <113288447+storm2107@users.noreply.github.com>
2023-01-31 23:42:30 +0200

    pac

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,6 +1,6 @@
 const ModalWindow = ({ largeImage, closeModal }) => {
   return (
-    <div className="overlay" onClick={() => closeModal()}>
+    <div className="overlay" onClick={() => closeModal()} onKeyDown={1}>
       <div className="modal">
         <img src={largeImage} alt="" />
       </div>
